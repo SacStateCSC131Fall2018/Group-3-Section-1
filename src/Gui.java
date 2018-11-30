2f049aca3cd4d078e81d57bad907a8de4fd8aa93
Colby LP <xyzaire@gmail.com>
2018-11-30 00:12:17 -0800

    Help menu is implemented
    Also looking at my code should be the fast track to other menus

--- src/Gui.java
+++ src/Gui.java
@@ -1,12 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
 
+//TODO Delete all imports at the end and then add them one by one because dark voodoo reasons
 public class Gui {
 
 	private JFrame window;
-	
-	//please put the menus into dropDownMenus
-	private JPanel dropDownMenus;
+	private JMenuBar menuBar;
+	private JMenu file;
+	private JMenu options;
+	private JMenu help;
 	
 	public static void main(String[] args) {
 		Gui gui = new Gui();
@@ -16,11 +18,31 @@ public class Gui {
 	public Gui()
 	{
 		window = new JFrame("Pirex: The faultier way of storing your archives");
-		dropDownMenus = new JPanel();
+		menuBar = new JMenuBar();
+		
+		file = new JMenu("File");
+		options = new JMenu("Options");
+		help = new JMenu("Help");
+		
+		JMenuItem index = new JMenuItem("Index");
+		JMenuItem about = new JMenuItem("About");
+		
+		about.addActionListener((event) -> JOptionPane.showMessageDialog(null, "Made by Group 3"));
+		
+		help.add(index);
+		help.add(about);
+		
+		menuBar.add(file);
+		menuBar.add(options);
+		menuBar.add(help);
+		
 		window.setLayout(new BorderLayout());
 		window.setVisible(true);
 		window.setSize(600, 400);
-		window.add(dropDownMenus);
+		window.add(menuBar, BorderLayout.NORTH);
+		
+		
+		
 	}
 	
 
