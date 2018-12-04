c046388c1d7306f11eff96683321f549dce198e1
Daniel Shevchyk <32968889+idaniki@users.noreply.github.com>
2018-12-04 10:46:33 -0800

    Update src/Gui.java

--- src/Gui.java
+++ src/Gui.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 //TODO Delete all imports at the end and then add them one by one because dark voodoo reasons
 public class Gui extends JFrame{
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private JMenuBar menuBar;
@@ -15,12 +15,12 @@ public class Gui extends JFrame{
 	private ImageIcon icon;
 	Container contentPane;
 	private JPanel mainContent;
-	
+
 	public static void main(String[] args) {
 		JFrame gui = new Gui();
 
 	}
-	
+
 	public Gui()
 	{
 		super("Pirex");
@@ -28,57 +28,61 @@ public class Gui extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane.setLayout(new BorderLayout());
 		this.setSize(600, 400);
-		
+
 		setIcon();
-		setupMenu();			
-		
+		setupMenu();
+
 		mainContent = new JPanel();
 		getContentPane().add(mainContent, BorderLayout.CENTER);
 		mainContent.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel buttonPanel = new JPanel();
 		mainContent.add(buttonPanel, BorderLayout.NORTH);
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-		
+
 		JButton searchBtn = new JButton("Search Documents");
 		buttonPanel.add(searchBtn);
-		
+
 		JButton loadBtn = new JButton("Load Documents");
 		buttonPanel.add(loadBtn);
-		
+
 		JButton summBtn = new JButton("Summarize Documents");
 		buttonPanel.add(summBtn);
 		this.setVisible(true);
 	}
-	
+
 	private void setIcon() {
 		URL iconUrl = Gui.class.getResource("assets/logo.png");
 		icon = new ImageIcon(iconUrl);
-		
+
 		this.setIconImage(icon.getImage());
 	}
-	
+
 	private void setupMenu() {
 		menuBar = new JMenuBar();
-		
+
 		file = new JMenu("File");
 		JMenuItem loadQuery = new JMenuItem("Load Query");
 		JMenuItem export = new JMenuItem("Export");
 		JMenuItem exit = new JMenuItem("Exit");
-		
+		JMenuItem documents = new JMenuItem("Documents");
+
 		options = new JMenu("Options");
-		
+
 		help = new JMenu("Help");
 		JMenuItem index = new JMenuItem("Index");
 		JMenuItem about = new JMenuItem("About");
-		
+
 		loadQuery.addActionListener((event) -> JOptionPane.showMessageDialog(null, "Function not available"));
+		documents.addActionListener((event) -> JOptionPane.showMessageDialog(null, "Function not available"));
 		export.addActionListener((event) -> JOptionPane.showMessageDialog(null, "Function not available"));
 		exit.addActionListener((event) -> System.exit(0));
 		file.add(loadQuery);
 		file.add(export);
 		file.add(exit);
-		
+
+		options.add(documents);
+
 		about.addActionListener((event) -> JOptionPane.showMessageDialog(null, "Made by Group 3"));
 		index.addActionListener((event) -> JOptionPane.showMessageDialog(null, "Function not available"));
 		help.add(index);
