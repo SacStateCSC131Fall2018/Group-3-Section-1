f6dcc11281d9af29733e8526f5e1e5ab75b3cbcc
Ildebrando Sagerdahl <ildebrandosagerdahl@csus.edu>
2018-12-04 18:18:30 -0800

    added search tab

--- src/Gui.java
+++ src/Gui.java
@@ -1,9 +1,13 @@
 import java.awt.*;
 import java.net.URL;
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 
 //TODO Delete all imports at the end and then add them one by one because dark voodoo reasons
-public class Gui extends JFrame{
+public class Gui extends JFrame {
 	/**
 	 *
 	 */
@@ -14,23 +18,22 @@ public class Gui extends JFrame{
 	private JMenu help;
 	private ImageIcon icon;
 	Container contentPane;
+	private JTextField queryField;
 
 	public static void main(String[] args) {
 		JFrame gui = new Gui();
 
 	}
 
-	public Gui()
-	{
+	public Gui() {
 		super("Pirex");
 		contentPane = getContentPane();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		contentPane.setLayout(new BorderLayout());
 		this.setSize(600, 400);
 
 		setIcon();
-		setupMenu();
 		setupPanels();
+		setupMenu();
 
 		this.setVisible(true);
 	}
@@ -44,6 +47,7 @@ public class Gui extends JFrame{
 
 	private void setupMenu() {
 		menuBar = new JMenuBar();
+		getContentPane().add(menuBar, BorderLayout.NORTH);
 
 		file = new JMenu("File");
 		JMenuItem loadQuery = new JMenuItem("Load Query");
@@ -74,20 +78,88 @@ public class Gui extends JFrame{
 		menuBar.add(file);
 		menuBar.add(options);
 		menuBar.add(help);
-		getContentPane().add(menuBar, BorderLayout.NORTH);
 	}
+
 	private void setupPanels() {
-		
+		getContentPane().setLayout(new BorderLayout(0, 0));
+
+		JPanel borderPanel = new JPanel();
+		borderPanel.setBorder(new EmptyBorder(0, 8, 8, 8));
+		getContentPane().add(borderPanel);
+		borderPanel.setLayout(new BorderLayout(0, 0));
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		getContentPane().add(tabbedPane, BorderLayout.CENTER);
-		
+		borderPanel.add(tabbedPane);
+
 		JPanel seachPanel = new JPanel();
 		tabbedPane.addTab("Search Documents", null, seachPanel, null);
-		
+		seachPanel.setLayout(new BorderLayout(0, 0));
+
+		JPanel queryPanel = new JPanel();
+		seachPanel.add(queryPanel, BorderLayout.NORTH);
+		queryPanel.setLayout(new BoxLayout(queryPanel, BoxLayout.X_AXIS));
+
+		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
+		horizontalStrut_1.setPreferredSize(new Dimension(10, 0));
+		queryPanel.add(horizontalStrut_1);
+
+		JLabel query = new JLabel("Query:");
+		query.setHorizontalAlignment(SwingConstants.LEFT);
+		queryPanel.add(query);
+
+		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
+		horizontalStrut_2.setPreferredSize(new Dimension(10, 0));
+		queryPanel.add(horizontalStrut_2);
+
+		queryField = new JTextField();
+		queryField.setHorizontalAlignment(SwingConstants.LEFT);
+		queryPanel.add(queryField);
+		queryField.setColumns(10);
+
+		JButton btnClear = new JButton("clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				queryField.setText("");
+			}
+		});
+
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		queryPanel.add(horizontalStrut);
+		btnClear.setHorizontalAlignment(SwingConstants.RIGHT);
+		queryPanel.add(btnClear);
+
+		JPanel textPanel = new JPanel();
+		textPanel.setBorder(new EmptyBorder(0, 25, 0, 25));
+		seachPanel.add(textPanel, BorderLayout.CENTER);
+		textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		textPanel.add(verticalStrut_1);
+
+		JTextArea textTop = new JTextArea();
+		textTop.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		textTop.setEditable(false);
+		textPanel.add(textTop);
+
+		Component verticalStrut = Box.createVerticalStrut(20);
+		textPanel.add(verticalStrut);
+
+		JTextArea textBot = new JTextArea();
+		textBot.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		textBot.setEditable(false);
+		textPanel.add(textBot);
+
+		Component verticalStrut_2 = Box.createVerticalStrut(20);
+		textPanel.add(verticalStrut_2);
+
+		Component horizontalStrut_4 = Box.createHorizontalStrut(20);
+		textPanel.add(horizontalStrut_4);
+
 		JPanel loadPanel = new JPanel();
 		tabbedPane.addTab("Load Documents", null, loadPanel, null);
+
+		JPanel summPanel = new JPanel();
+		tabbedPane.addTab("Summarize Documents", null, summPanel, null);
 		
-			JPanel summPanel = new JPanel();
-			tabbedPane.addTab("Summarize Documents", null, summPanel, null);
 	}
 }
